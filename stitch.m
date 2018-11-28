19cb0ce2881ea6d7992cbf2038cfdd674777b739
Prakyath <43697963+prakyath-04@users.noreply.github.com>
2018-11-28 14:14:17 +0530

    Updating File

--- stitch.m
+++ stitch.m
@@ -53,25 +53,73 @@ xLimits = [xMin xMax];
 yLimits = [yMin yMax];
 panoramaView = imref2d([height width], xLimits, yLimits);
 
-% Create the panorama.
-for i = 1:len
-    
+I = img_arr{1};
+warpedImage = imwarp(I, h(1), 'OutputView', panoramaView);
+mask = imwarp(true(size(I,1),size(I,2)), h(1), 'OutputView', panoramaView);
+panorama = step(blender,panorama,warpedImage,mask);
+siz = size(panorama);
+for i = 2:len
     I = img_arr{i};
-    
     % Transform I into the panorama.
     warpedImage = imwarp(I, h(i), 'OutputView', panoramaView);
-    
     % Generate a binary mask.
     mask = imwarp(true(size(I,1),size(I,2)), h(i), 'OutputView', panoramaView);
-    
     % Overlay the warpedImage onto the panorama.
-    panorama = step(blender, panorama, warpedImage, mask);
-%     panorama1 = (panorama1/2+step(blender1,panorama1,warpedImage)/2);
-%     panorama2 = (panorama1-step(blender1,panorama1,warpedImage)) ;
-%     panorama1 = panorama1 + panorama2;
-%     panorama1 = step(blender1,panorama1,warpedImage);
-%     imshow(panorama1);
-%     pause(2);
+    %     panorama = step(blender,panorama,warpedImage,mask);
+    %     panorama = max(panorama,step(blender, panorama, warpedImage));
+%     mask1 = mask;
+%     flag1 = 0;
+%     flag2 = 0;
+%     x2 = 0;
+%     y2 = 0;
+%     x3 = 0;
+%     y3 = 0;
+    
+%     for j=1:siz(1)
+%         for k=1:siz(2)
+%             if mask(j,k) == 1
+%                 if (panorama(j,k,1) == 0 && panorama(j,k,2) == 0 && panorama(j,k,3)==0)
+%                     mask1(j,k) = 0;
+%                 else
+%                     mask1(j,k) = 1;
+%                 end
+%             end
+%         end
+%     end
+% %     
+%     for j=1:siz(1)
+%         for k=1:siz(2)
+%             if (mask1(j,k) == 1 && flag1 ==0 && flag2 ==0)
+%                     x2 = j;
+%                     y2 = k;
+%                     flag1 = 1; 
+%             end
+%             if (mask1(j,k) == 0 && flag1 ==1 && flag2 ==0)
+%                 x3 = j;
+%                 y3 = k;
+%                 flag2 = 1;
+%                 break;
+%             end
+%         end
+%         if (flag2 == 1)
+%             break;
+%         end
+%     end
+    w1 = 0.5;
+    w2 = 0.5;
+    for j=1:siz(1)
+        for k=1:siz(2)
+            if mask(j,k) == 1
+                if (panorama(j,k,1) == 0 && panorama(j,k,2) == 0 && panorama(j,k,3)==0)
+                    panorama(j,k,:) = warpedImage(j,k,:);
+                else
+%                     w1 = sqrt((j-x3)*(j-x3) + (k-y3)*(k-y3));
+%                     w2 = sqrt((j-x2)*(j-x2) + (k-y2)*(k-y2));
+                    panorama(j,k,:) = panorama(j,k,:)*(w2/(w2+w1)) + warpedImage(j,k,:)*(w1/(w1+w2));
+                end
+            end
+        end
+    end
 end
 pano = panorama;
 end
